42aa81530b72f3b4b25d92f15b8d76881bf4265d
Andrew Gilliland <andrewpgilliland@gmail.com>
2026-01-07 22:58:45 -0600

    refactor: styles and magic nums into const

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,3 +3,12 @@ export const HEADER_HEIGHT = 80;
 export const SCROLL_OFFSET = 24;
 
 export const SECTION_TOP_PADDING_OFFSET = HEADER_HEIGHT + SCROLL_OFFSET;
+
+export const CIRCLE_SIZES = {
+  SMALL: 100,
+  MEDIUM: 250,
+  LARGE: 434,
+} as const;
+
+export const LESSON_NAV_BUTTON_CLASSES =
+  "text-green-500 font-semibold text-lg hover:underline hover:underline-green-500 border-green-500 border-2 px-2 py-1 rounded-[10px]";
