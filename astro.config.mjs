d00bcde27bfdabb6433c2c8ff21970a7aec7932c
Andrew Gilliland <andrewpgilliland@gmail.com>
2026-01-21 21:34:52 -0600

    feat: adds clientrouter to layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
-import cloudflare from "@astrojs/cloudflare";
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: "static",
+  output: 'static',
   adapter: cloudflare(),
   integrations: [react(), tailwind(), mdx()],
 });
